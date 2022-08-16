425d2f474ae1ca37a9c8377becb80e0eba59154c
Abdul Mueed <48639552+AbdulMueed11@users.noreply.github.com>
2022-08-16 09:15:47 +0500

    created update function

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get('/', (req, res) => {
-    res.send("hey its user route")
+// update user
+router.put("/:id", async (req, res) => {
+    if (req.body.userid === req.params.id || req.user.isAdmin) {
+        if(req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }
+            catch(err) {
+                return err.status(500).json(err);
+            }
+        }
+    } else {
+        return res.status(403).json("You can update only your account!");
+    }
 })
+// delete user
+// get user
+// follow user
+// unfollow user
 
 module.exports = router
